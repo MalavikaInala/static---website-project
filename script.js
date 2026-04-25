db1e2af6264aa6e52adfb9a0296d4cb5d4e06a53
MalavikaInala <malavikainala@gmail.com>
2026-04-25 09:22:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function sayHi() {
+  document.getElementById('output').innerText = 'JS is working! Site deployed at ' + new Date().toLocaleTimeString();
+  console.log('S3 static site loaded successfully');
+}
